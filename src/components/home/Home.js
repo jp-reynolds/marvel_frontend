ff96b3bcd85baa8f66bae24f950622175cb52d58
jp-reynolds <jpreynolds215@gmail.com>
2018-05-15 08:11:26 -0600

    added hover styling to every page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,10 +13,30 @@ class Home extends Component {
       		<ReactPlayer width="auto" url='https://www.youtube.com/embed/CEP-PObKELA' playing />
       		<div className="homeButtonContainer">
       			<ul className="responsiveButtons">
-      				<a className="homeObject" href='/heroes'><img className="homeImage" id="heroes" src="https://www.screengeek.net/wp-content/uploads/2017/09/marvel-cinematic-universe-easter-eggs.jpg" /></a>
-      				<a className="homeObject" href='/villains'><img className="homeImage" id="villains" src="http://www.screengeek.net/wp-content/uploads/2016/07/Marvel-Comics-Villains-List.jpg" /></a>
-      				<a className="homeObject" href='/places'><img className="homeImage" id="locations" src="https://wi-images.condecdn.net/image/GEp1VNDqw66/crop/1620/f/marvelmap.jpg" /></a>
-      				<a className="homeObject" href='/#'><img className="homeImage" id="theories" src="https://c1.staticflickr.com/5/4738/39138378452_57b59f66e4_b.jpg" /></a>
+      				<a className="homeObject" href='/heroes'>
+                <div className="buttonContainer">
+                  <img className="homeImage" id="heroes" src="https://www.screengeek.net/wp-content/uploads/2017/09/marvel-cinematic-universe-easter-eggs.jpg" />
+                  <h1 className="centered"> Heroes</h1>
+                </div>
+              </a>
+      				<a className="homeObject" href='/villains'>
+                <div className="buttonContainer">
+                  <img className="homeImage" id="villains" src="http://www.screengeek.net/wp-content/uploads/2016/07/Marvel-Comics-Villains-List.jpg" />
+                  <h1 className="centered"> Villains</h1>
+                </div>
+              </a>
+      				<a className="homeObject" href='/places'>
+              <div className="buttonContainer">
+                <img className="homeImage" id="locations" src="https://wi-images.condecdn.net/image/GEp1VNDqw66/crop/1620/f/marvelmap.jpg" />
+                <h1 className="centered"> Locations</h1>
+              </div>
+              </a>
+      				<a className="homeObject" href='/#'>
+              <div className="buttonContainer">
+                <img className="homeImage" id="theories" src="https://c1.staticflickr.com/5/4738/39138378452_57b59f66e4_b.jpg" />
+                <h1 className="centered"> Theories</h1>
+              </div>
+              </a>
       			</ul>
       			<img className="movies" src="https://i.ytimg.com/vi/dRe_PBAIIiw/maxresdefault.jpg"/>
       		</div>
